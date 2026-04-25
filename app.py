900893c3758c3a6998e54053587a1d3018a4f460
deepxjaat <deepxjaat@gmail.com>
2026-04-25 22:01:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -157,5 +157,7 @@ def update_status(id):
     conn.commit()
     return "Done"
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
